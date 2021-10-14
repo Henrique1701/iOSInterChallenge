e64bab8a55deedb0713ebd363a278e510ee958cb
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-10-14 18:42:38 -0300

    add UI test

--- InterChallengeUITests/InterChallengeUITests.swift
+++ InterChallengeUITests/InterChallengeUITests.swift
@@ -1,35 +1,39 @@
 import XCTest
 
 class InterChallengeUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
+        self.app = XCUIApplication()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.app = nil
+        try super.tearDownWithError()
     }
+    
+    func testChallengeScreenExists() {
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+        let challengeText = app.navigationBars["Desafio"].staticTexts["Desafio"]
+        XCTAssertNotNil(challengeText, "The title of the nagitation bar does not exist")
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tablesQuery = app.tables
+
+        let bretLabel = tablesQuery.staticTexts["Bret"]
+        XCTAssertNotNil(bretLabel, "The Bret label does not exist")
+
+        let antonetteLabel = tablesQuery.staticTexts["Antonette"]
+        XCTAssertNotNil(antonetteLabel, "The Antonette label does not exist")
+
+        let samanthaLabel = tablesQuery.staticTexts["Samantha"]
+        XCTAssertNotNil(samanthaLabel, "The Samantha label does not exist")
+
+        let karianneLabel = tablesQuery.staticTexts["Karianne"]
+        XCTAssertNotNil(karianneLabel, "The bret label does not exist")
+        
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    
 }
